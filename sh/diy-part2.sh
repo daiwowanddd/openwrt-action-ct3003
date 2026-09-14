1b92949a529645790001a2016fc41e1deb48f7cb
miaomiao <130832744+daiwowanddd@users.noreply.github.com>
2026-09-14 13:31:40 +0800

    Update OpenClash repository branch to main

--- sh/diy-part2.sh
+++ sh/diy-part2.sh
@@ -20,5 +20,4 @@ sed -i 's/192.168.6.1/192.168.10.1/g' package/base-files/files/bin/config_genera
 #sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
 
 git clone -b main --single-branch --filter=blob:none https://github.com/sirpdboy/luci-app-adguardhome package/luci-app-adguardhome
-git clone -b master --single-branch --filter=blob:none https://github.com/vernesong/OpenClash package/luci-app-openclash
 #sed -i 's/--set=llvm\.download-ci-llvm=true/--set=llvm.download-ci-llvm=false/' feeds/packages/lang/rust/Makefile
